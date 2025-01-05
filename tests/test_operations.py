fc8050f1857724e422dcb0221e969781f1747c04
Anuj Kothiyal <anujkothiyal01@gmail.com>
2025-01-05 16:39:19 +0530

    Updated Test for sub

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -5,7 +5,7 @@ def test_add():
     assert add(-1, 1) == 0
 
 def test_sub():
-    assert(5, 3) == 2
-    assert(4, 1) == 3
-    assert(5, 5) == 0
-    assert(2, 3) == -1
+    assert sub(5, 3) == 2
+    assert sub(4, 1) == 3
+    assert sub(5, 5) == 0
+    assert sub(2, 3) == -1
